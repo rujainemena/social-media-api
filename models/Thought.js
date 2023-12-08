35f88c8622fc93177c30925db62b5fa00a56f475
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-12-08 16:44:57 -0500

    thought model and reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reactions');
+
+// Schema to create Post model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get:(newDate)=>newDate.toLocaleDateString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `reactions` that gets the amount of reactions per thought / comment. 
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -4,13 +4,13 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
   {
     username: {
-      type: string,
+      type: String,
       unique: true,
       required: true,
       trim:true
     }, 
     email: {
-      type: string,
+      type: String,
       unique: true,
       match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
     },
