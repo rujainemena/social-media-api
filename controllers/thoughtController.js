ecbd324c8ac057a24efa03dd0007530eb156d821
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-12-08 21:09:42 -0500

    fixed bug with single thought route

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -10,10 +10,10 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  //  get single thought by username
+  //  get single thought by id
   async getSingleThought(req, res) {
     try {
-      const thought = await Thought.findOne({ username: req.body.username })
+      const thought = await Thought.findOne({ _id: req.params.thoughtId })
 
       if (!thought) {
         return res.status(404).json({ message: 'No thought with that username.' });
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,17 +12,17 @@ const {
 // http://localhost:3001/api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// http://localhost:3001/api/thoughts/username
+// http://localhost:3001/api/thoughts/:thoughtId
 router
-  .route('/username')
+  .route('/:thoughtId')
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
 
-// // http://localhost:3001/api/thoughts/username/reactions
-router.route('/username/reactions').post(addReaction);
+// // http://localhost:3001/api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
 
-// // http://localhost:3001/api/thoughts/username/reactions/:reactionId
-router.route('/username/reactions/:reactionId').delete(removeReaction);
+// // http://localhost:3001/api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
