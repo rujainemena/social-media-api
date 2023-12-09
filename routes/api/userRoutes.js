d707a6665d16c047c8b6f829111fa36ce27526de
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-12-08 19:20:01 -0500

    changed database

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -11,12 +11,19 @@ const {
 
 // /api/users
 //http://localhost:3001/api/users/
-router.route("/").get(getUsers).post(createUser);
+router.route("/")
+.get(getUsers)
+.post(createUser);
 
 // /api/users/:userId
-router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route("/:userId")
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+router.route("/:userId/friends/:friendId")
+.post(addFriend)
+.delete(removeFriend);
 
 module.exports = router;
